71d42d935970a7b61e79996e00c0092b20388748
punipuni21 <watanabe-ryotaro-yp@ynu.jp>
2020-06-16 19:31:09 +0900

    add makeLabel.py

---
+++ makeLabel.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 17:12:34 2020
+
+@author: ShimaLab
+"""
+from PIL import Image
+import numpy as np
+import os, os.path
+from tkinter import filedialog
+import sys
+import pathlib
+
+#--------------------------------------------------------------------------------------#
+##ラベルを保存するためのディレクトリ
+def makeDir(path_pwd):
+    
+    
+    dirLabelWhite=path_pwd
+    dirLabelColor=path_pwd
+    
+    ##ラベルを保存するディレクトリの指定
+    if path_pwd[-1] != '\\':
+        dirLabelWhite = path_pwd + '\\Label\\'
+        dirLabelColor = path_pwd + '\\Label\\'
+    if not os.path.exists(dirLabelWhite+"White"):
+        os.mkdir(dirLabelWhite+"White")
+
+    dirLabelWhite = dirLabelWhite + 'White\\'
+    
+    if not os.path.exists(dirLabelColor+"Color"):
+        os.mkdir(dirLabelColor+"Color")
+    dirLabelColor = dirLabelColor + 'Color\\'
+    
+    return (dirLabelWhite,dirLabelColor)
+
+#--------------------------------------------------------------------------------------#
+#病変のディレクトリにあるファイル数を取得
+def searchtheNumberOfFiles(currentDir):
+    
+    theNumberOfFiles={}
+    for dir_path in os.listdir(parentDir):
+        target_dir = parentDir + "\\" + dir_path
+        files = os.listdir(target_dir)
+        count = len(files)
+        theNumberOfFiles[dir_path]=count
+    return theNumberOfFiles
+#--------------------------------------------------------------------------------------#
+def isLesionSizeSame(dirName, lesionList):
+    
+    #このプログラムはhoge\srcにあるので病変のサイズを取得するためには親ディレクトリ内で探すこと
+    theNumberOfFiles=searchtheNumberOfFiles(parentDir)
+  
+    #各病変の個数を格納する変数
+    theNumberOfEachLesionList=[]
+    for LesionName in lesionList:
+        theNumberOfEachLesionList.append(theNumberOfFiles[LesionName])
+    
+    
+    theNumberOfEachLesion=theNumberOfEachLesionList[0]
+#    theNumberOfEachLesionList[0]=8 #これはデバッグ用
+    
+    #病変のファイルの個数が全て同じでない場合は何かがおかしいのでプログラムを終了
+    return (any(i != theNumberOfEachLesion for i in theNumberOfEachLesionList),theNumberOfEachLesion)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    
+    #--------------------------------------------------------------------------------#
+    #病変リスト
+    lesionList=["Haemorrhages","HardExudates","Microaneurysms","SoftExudates"]
+    #画像の情報
+    Height,Width=(512,512)
+    #--------------------------------------------------------------------------------#
+    
+    
+    print("読込ファイル")
+    currentDir = os.path.abspath(os.path.dirname(__file__))
+        
+    path_pwd_pathlib = pathlib.Path(os.getcwd())#カレントディレクトリの取得,strではない
+    parentDir=str(path_pwd_pathlib.parent)#親ディレクトリの取得
+    
+    #ラベルを保存するディレクトリの作成（白黒ラベル，カラーラベル）
+    (dirLabelWhite,dirLabelColor) = makeDir(path_pwd=parentDir)
+    
+    
+    #病変の個数が全て同じか判定，同じでないならプログラム終了
+    isEachLesionSizeSame, theNumberOfEachLesion=isLesionSizeSame(parentDir, lesionList)
+    if(isEachLesionSizeSame):
+        sys.exit("病変の個数が全て統一されていない，各病変のディレクトリを確認してください")
+    
+    #ここからマスクを生成するプログラム
+    
+    whiteMask=np.zeros((theNumberOfEachLesion,Height,Width,3),'uint8')#白黒マスク(RoleNet前段部用)
+    colorMask=np.zeros((theNumberOfEachLesion,Height,Width,3),'uint8')#色マスク（RoleNet後段部用）
+    colorDict={"Haemorrhages":np.array([255,0,0],'uint8'),"HardExudates":np.array([0,255,0],'uint8'),\
+               "Microaneurysms":np.array([0,0,255],'uint8'),"SoftExudates":np.array([255,255,0],'uint8')}#赤，緑，青，黄
+    
+    
+    
+    #マスクを求める部分の実装はまだできていない
+    for idx in range(theNumberOfEachLesion):
+        
+        for lesionName in lesionList:
+            
+            npyFile=parentDir+'\\npyFiles\\'+lesionName+'.npy'
+            hoge=np.load(npyFile)
+            #黒の部分は何もしない
+            #赤の部分は白にしてマスクを作る（白黒）
+            #現画像の赤い部分はcolorDictの色で置換してマスクを作成（色マスク）
+            break
+        break
+    
+    
+    ##保存
+    np.save(dirLabelWhite+"whiteMask",whiteMask)
+    np.save(dirLabelColor+"colorMask",colorMask)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ tmp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 18:06:08 2020
+
+@author: ShimaLab
+"""
+
+import pathlib
+import os, os.path
+
+if __name__ == '__main__':
+
+    p_sub = pathlib.Path(os.getcwd())
+    par=str(p_sub.parent)
